db8efc92f9d255134ea2207fb3e0d4fbe3b3cea4
Tarjei Utnes <tutnes@gmail.com>
2017-01-08 15:57:50 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const edge = require('edge');
+
+
+ 
+var helloWorld = edge.func(function () {/*
+    async (input) => { 
+        return ".NET Welcomes " + input.ToString(); 
+    }
+*/});
+ 
+helloWorld('JavaScript', function (error, result) {
+    if (error) throw error;
+    console.log(result);
+});
